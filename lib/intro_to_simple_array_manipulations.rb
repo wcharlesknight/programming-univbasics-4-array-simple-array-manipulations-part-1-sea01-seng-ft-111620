94bbf1f6a26d13785647cd621fd2de6e1b730673
Charlie Knight <williamchknight@gmail.com>
2020-11-05 19:03:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,42 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, string)
+ bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+ new_neighborhood = "Staten Island"
+ bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+using_pop(continents)
+
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+pop_with_args(dog_breeds)
+
+def using_shift(array)
+  array.shift 
+end
+
+my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+
+using_shift(my_favorite_cities)
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
+shift_with_args(ice_cream_brands)
