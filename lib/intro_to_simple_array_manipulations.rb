b20b682063fb8bee5d2d82db24821273ac51c6c1
Charlie Knight <williamchknight@gmail.com>
2020-11-05 18:56:38 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array, string)
+ bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+ new_neighborhood = "Staten Island"
+ bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+
+using_pop(continents)
+
+
+def pop_with_args(array)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = ["Chihuahua","Shiba Inu"]
+  dog_breeds.pop(small_dogs)
+end
+
+
+
+
